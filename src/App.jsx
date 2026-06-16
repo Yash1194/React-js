746d7c12bf5967d71a1291baf05bc2dc6b35ee6f
Yash Swarnkar <ys703981@gmail.com>
2026-06-16 18:22:08 +0530

    useEffect

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+
+const App = () => {
+  const [username, setUsername] = useState("")
+  const [num, setNum] = useState(0)
+  
+  const getData = async () => {
+    const response = await axios.get('https://randomuser.me/api/')
+    setUsername(response.data.results[0].name.first)
+  }
+  useEffect(() => {
+    getData();
+  }, [num]);
+  return (
+    <div>
+      {username}
+      <h1>{num}</h1>
+      <button onClick={()=>{
+        setNum(num + 1)
+      }}>click here </button>
+    </div>
+  )
+}
+
+export default App
